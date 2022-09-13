66d9bd53118e098b2167eece3f20b1e6c7e8c686
LLIaluH <drunya2999@mail.ru>
2022-09-13 10:17:50 +0300

    FixNewGame

--- 2048/Form1.cs
+++ 2048/Form1.cs
@@ -55,8 +55,10 @@ namespace _2048
                 {
                     Controls.Remove(item);
                     item.Dispose();
+                    i--;
                 }
             }
+
             int Tag = 0;
             for (int y = 0; y < 4; y++)
             {
